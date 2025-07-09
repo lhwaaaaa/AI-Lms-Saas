66616eb7d90d38fdb533430d2d32bcc95cb2962c
Mehtaab <mehtaabaalam117@gmail.com>
2025-07-09 08:19:26 +0530

    Add TypeScript definitions for companions and messaging system
    - Introduced enums for Subject and Message types to standardize values.
    - Defined interfaces for Companion, CreateCompanion, GetAllCompanions, and CreateUser to structure data.
    - Added Avatar and SavedMessage interfaces for user-related functionalities.
    - Created message types including TranscriptMessage, FunctionCallMessage, and FunctionCallResultMessage to handle different message formats in the messaging system.

---
+++ app/companions/new/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewCompanions = () => {
+  return (
+    <div>NewCompanions</div>
+  )
+}
+
+export default NewCompanions
---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Button} from "@/components/ui/button";
+
+const Page = () => {
+    return (
+        <div className="text-2xl underline">
+            <h1>Hello from my saas app</h1>
+            <Button>Lets get started</Button>
+        </div>
+
+    )
+}
+
+export default Page
