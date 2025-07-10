447fedf90f34cd81a8f9cdf42bc2e4cc766abda2
Mehtaab <mehtaabaalam117@gmail.com>
2025-07-10 16:36:54 +0530

    Clerk authentication + Sign-in page customisation

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/Navbar";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const bricolage = Bricolage_Grotesque({
   variable: "--font-bricolage",
@@ -19,14 +20,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${bricolage.variable} antialiased`}>
+    <ClerkProvider appearance={{ variables: { colorPrimary: "#fe5933" } }}>
+      <html lang="en">
+        <body className={`${bricolage.variable} antialiased`}>
 
-        <Navbar />
-        {children}
+          <Navbar />
+          {children}
 
 
-      </body>
-    </html>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import NavItems from './NavItems'
+
+import { SignInButton, SignUpButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+
 const Navbar = () => {
     return (
         <nav className='navbar'>
@@ -19,6 +22,16 @@ const Navbar = () => {
 
             <div className='flex items-center gap-8'>
                 <NavItems />
+
+                <SignedOut>
+                        <SignInButton >
+                            <button className='btn-signin'>Sign In</button>
+                        </SignInButton>
+                   
+                </SignedOut>
+                <SignedIn>
+                    <UserButton afterSignOutUrl='/' />
+                </SignedIn>
             </div>
         </nav>
     )
