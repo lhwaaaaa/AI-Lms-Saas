1eb9954951770115613fc40eaa760a86351eebc3
Mehtaab <mehtaabaalam117@gmail.com>
2025-07-10 07:45:49 +0530

    Implement Companion Builder form using react-hook-forms & Zod and various component from shadcn

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,8 +1,16 @@
+import CompanionForm from '@/components/CompanionForm'
 import React from 'react'
 
 const NewCompanions = () => {
   return (
-    <div>NewCompanions</div>
+    <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
+      <article className='w-full flex flex-col gap-4'>
+        <h1>
+          Companion Builder
+        </h1>
+        <CompanionForm />
+      </article>
+    </main>
   )
 }
 
