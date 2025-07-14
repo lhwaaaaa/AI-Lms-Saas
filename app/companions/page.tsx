877ac3f479c9e749012552700f900adc30bacbb5
Mehtaab <mehtaabaalam117@gmail.com>
2025-07-14 17:17:26 +0530

    Get all companion action

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import { getAllCompanions } from "@/lib/actions/companion.actions";
 
-const Companions = () => {
+const Companions = async ({ searchParams }: SearchParams) => {
+
+  const filters = await searchParams;
+
+  const subject = filters.subject ? filters.subject : '';
+  const topic = filters.topic ? filters.topic : '';
+
+
+  const companions = await getAllCompanions({subject, topic})
+  console.log(companions)
   return (
     <div>Companions</div>
   )
