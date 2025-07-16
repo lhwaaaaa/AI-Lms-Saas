e40f910192d6bad199c2c7352d72fc64bc6c3dc5
Mehtaab <mehtaabaalam117@gmail.com>
2025-07-16 19:18:08 +0530

    Enhance CompanionComponent with Lottie animations and microphone toggle functionality

--- components/CompanionComponent.tsx
+++ components/CompanionComponent.tsx
@@ -2,8 +2,10 @@
 
 import { cn, getSubjectColor } from "@/lib/utils"
 import { vapi } from "@/lib/vapi.sdk"
-import { useEffect, useState } from "react"
-
+import Lottie, { LottieComponentProps, LottieRefCurrentProps } from "lottie-react"
+import Image from "next/image"
+import { useEffect, useRef, useState } from "react"
+import soundwaves from '../constants/soundwaves.json'
 enum CallStatus {
     INACTIVE = 'INACTIVE',
     ACTIVE = 'ACTIVE',
@@ -15,6 +17,36 @@ const CompanionComponent = ({ companionId, subject, title, topic, name, userName
 
     const [callStatus, setCallStatus] = useState<CallStatus>(CallStatus.INACTIVE)
     const [isSpeaking, setIsSpeaking] = useState(false)
+    const [isMuted, setIsMuted] = useState(false)
+
+    const lottieRef = useRef<LottieRefCurrentProps>(null)
+
+    const toggleMicrophone = () => {
+        const isMuted = vapi.isMuted();
+        vapi.setMuted(!isMuted)
+        setIsMuted(!isMuted)
+    }
+
+    const handleCall = async () => {
+
+    }
+    const handleDisconnect  = async () => {
+
+    }
+
+
+    useEffect(() => {
+
+        if (lottieRef) {
+            if (isSpeaking) {
+                lottieRef.current?.play()
+            } else {
+                lottieRef.current?.stop()
+            }
+        }
+
+    }, [isSpeaking, lottieRef])
+
 
     useEffect(() => {
         const onCallStart = () => setCallStatus(CallStatus.ACTIVE)
@@ -31,7 +63,19 @@ const CompanionComponent = ({ companionId, subject, title, topic, name, userName
         vapi.on('call-end', onCallEnd)
         vapi.on('message', onMessage)
         vapi.on('error', onError)
+        vapi.on('speech-end', onSpeechEnd)
+        vapi.on('speech-start', onSpeechStart)
 
+
+        return () => {
+            vapi.off('call-start', onCallStart)
+            vapi.off('call-end', onCallEnd)
+            vapi.off('message', onMessage)
+            vapi.off('error', onError)
+            vapi.off('speech-end', onSpeechEnd)
+            vapi.off('speech-start', onSpeechStart)
+
+        }
     }, [])
 
 
@@ -43,8 +87,42 @@ const CompanionComponent = ({ companionId, subject, title, topic, name, userName
                 <div className="companion-section">
 
                     <div className="companion-avatar" style={{ backgroundColor: getSubjectColor(subject) }}>
-                        <div className={cn('absolute transition-opacity duration-1000')}></div>
+                        <div className={cn('absolute transition-opacity duration-1000', callStatus === CallStatus.FINISHED || callStatus === CallStatus.INACTIVE ? 'opacity-100' : 'opacity-0', callStatus === CallStatus.CONNECTING && 'opacity-100 animate-pulse')}>
+
+
+                            <Image src={`/icons/${subject}.svg`} alt="Subject" width={150} height={150} className="max-sm:w-fit " />
+                        </div>
+
+
+                        <div className={cn('absolute transition-opacity duration-1000 ', callStatus === CallStatus.ACTIVE ? 'opacity-100' : 'opacity-0')}>
+
+                            <Lottie
+                                lottieRef={lottieRef}
+                                animationData={soundwaves}
+                                autoplay={false}
+                                className="companion-lottie"
+                            />
+
+                        </div>
                     </div>
+
+
+                    <p className="font-bold text-2xl">{name}</p>
+                </div>
+                <div className="user-section">
+                    <div className="user-avatar">
+                        <Image src={userImage} alt="User image" width={130} height={130} />
+                        <p className="font-bold text-2xl">{userName}</p>
+                    </div>
+                    <button className="btn-mic" onClick={toggleMicrophone}> <Image src={isMuted ? '/icons/mic-off.svg' : '/icons/mic-on.svg'} alt="mic" width={36} height={36} />
+                        <p className="max-sm:hidden">{isMuted ? 'Turn on Mic' : 'Turn off Mic'}</p>
+                    </button>
+                    <button className={cn('rounded-lg text-white transition-colors w-full py-2 cursor-pointer', callStatus === CallStatus.ACTIVE ? 'bg-red-700' : 'bg-primary', callStatus === CallStatus.CONNECTING && 'animate-pulse')} onClick={callStatus === CallStatus.ACTIVE ? handleDisconnect : handleCall}>
+
+
+                        {callStatus === CallStatus.ACTIVE ? 'End Session' : callStatus === CallStatus.CONNECTING ? 'Connecting' : 'Start Session'}
+
+                    </button>
                 </div>
 
             </section>
